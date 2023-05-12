3b637a28d9e64c685c70e25f0622fdcf6e9015ea
Ovchinin <ovchinin@progressivemedia.ru>
2023-05-12 13:49:27 +0300

    Laravel 9

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('about',[\App\Http\Controllers\AboutController::class,'about']);
